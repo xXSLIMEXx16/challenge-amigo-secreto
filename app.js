7623aa8115cbd59b16cfc1b8a88624555c5cb3ef
Mario Arana <mariorobertoarana@gmail.com>
2025-08-14 01:12:35 -0300

    agregarda la funcion de limpiar caja

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos =[];
 
+/// Funcion para limpiar la caja ///
+function limpiarCaja(){
+    let valorCaja = document.getElementById('amigo').value = '';;
+
+}
+
+//// Funcion para agregar los nombres a la lista /// 
+
 function agregarAmigo() {
     let nombreamigo = document.getElementById('amigo').value.trim();
     if (nombreamigo === '') {
         alert("Por favor, inserte un nombre.");
     } else {
-        // Aquí pones lo que quieres hacer con el nombre válido
         amigos.push(nombreamigo);
+        limpiarCaja();
     }
 }
 
