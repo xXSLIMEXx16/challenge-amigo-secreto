507f2770021abc939c562e009cd1cb5e4e6f6747
Mario Arana <mariorobertoarana@gmail.com>
2025-08-14 01:34:21 -0300

    varias funcionalidades agregadas

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos =[];
 
+/// Agrega los nombres a la lista ///
+function mostrarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i]; 
+
+        lista.appendChild(li);
+    }
+}
+
 /// Funcion para limpiar la caja ///
 function limpiarCaja(){
     let valorCaja = document.getElementById('amigo').value = '';
@@ -16,6 +28,20 @@ function agregarAmigo() {
     } else {
         amigos.push(nombreamigo);
         limpiarCaja();
+        mostrarLista(); 
     }
+}
+
+/// Funcion para sortear amigos ///
+function sortearAmigo() {
+
+    if (amigos.length > 0) {
+    let nombre_aleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[nombre_aleatorio];
+    document.getElementById('resultado').innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+    }else {
+        alert("Lista vacia");
+}
+
 }
 
